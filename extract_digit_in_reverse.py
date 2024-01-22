2bbd8f9b02682869f65a362562ed77642b1f0f5d
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 23:53:47 +0800

    def function and print

--- extract_digit_in_reverse.py
+++ extract_digit_in_reverse.py
@@ -2,6 +2,9 @@
 #For example, If the given int is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
 
 #Integer to string and reverse it
+def reverse_digits(number):
+    reversed_string = str(number)[::-1]
 
 #Print each digit with space separating them
-
+    result = ' '.join(reversed_string)
+    print(result)

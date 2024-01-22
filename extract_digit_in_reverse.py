a03324bb9e629193023963902177c6a5df2820b7
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 23:45:05 +0800

    pseudocode

--- extract_digit_in_reverse.py
+++ extract_digit_in_reverse.py
@@ -1,3 +1,7 @@
 #Exercise 11: Write a Program to extract each digit from an integer in the reverse order.
 #For example, If the given int is 7536, the output shall be “6 3 5 7“, with a space separating the digits.
 
+#Integer to string and reverse it
+
+#Print each digit with space separating them
+
